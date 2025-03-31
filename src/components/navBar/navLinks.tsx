8f2d347e9f2d31ba3482718c69aecbb199779c2b
The-isaacagboola <isaacadeyemi9@gmail.com>
2025-03-31 14:46:24 +0100

    Adding a Nav BAr

--- src/components/navBar/navLinks.tsx
+++ src/components/navBar/navLinks.tsx
@@ -1,36 +1,54 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const NavLinks = () => {
-  const [active, setActive] = useState("/active");
+  const pathname = usePathname();
+
   return (
-    <ul className="flex gap-2">
-      <li
-        onClick={() => setActive("/matches")}
-        className={`${
-          active == "/matches" ? "text-green-700 font-semibold" : ""
-        }`}
-      >
-        <Link href={"/matches"}>Matches</Link>
-      </li>
-      <li
-        onClick={() => setActive("/friends")}
-        className={`${
-          active == "/friends" ? "text-green-700 font-semibold" : ""
-        }`}
-      >
-        <Link href={"/friends"}> Lists</Link>
-      </li>
-      <li
-        onClick={() => setActive("/messages")}
-        className={`${
-          active == "/messages" ? "text-green-700 font-semibold" : ""
-        }`}
-      >
-        <Link href={"/messages"}>Messages</Link>
-      </li>
-    </ul>
+    <div className="flex items-center  gap-10 w-[40%] justify-between">
+      <ul className="flex gap-2">
+        <li
+          className={`${
+            pathname == "/matches" ? "text-green-700 font-semibold" : ""
+          }`}
+        >
+          <Link href={"/matches"}>Matches</Link>
+        </li>
+        <li
+          className={`${
+            pathname == "/friends" ? "text-green-700 font-semibold" : ""
+          }`}
+        >
+          <Link href={"/friends"}> Lists</Link>
+        </li>
+        <li
+          className={`${
+            pathname == "/messages" ? "text-green-700 font-semibold" : ""
+          }`}
+        >
+          <Link href={"/messages"}>Messages</Link>
+        </li>
+      </ul>
+
+      <div className="flex gap-4">
+        <button
+          className={`${
+            pathname == "/login" ? "text-green-700 font-semibold" : ""
+          }`}
+        >
+          <Link href={"/login"}>Login</Link>
+        </button>
+        <button
+          className={`${
+            pathname == "/register" ? "text-green-700 font-semibold" : ""
+          }`}
+        >
+          <Link href={"/register"}>Register</Link>
+        </button>
+      </div>
+    </div>
   );
 };
 
