5824b5f7587ae7592cf716b8c6812ddeafa16626
The-isaacagboola <isaacadeyemi9@gmail.com>
2025-04-25 18:36:59 +0100

    fixing build errors

--- src/app/messages/layout.tsx
+++ src/app/messages/layout.tsx
@@ -1,10 +1,12 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import MessageSideBar from "./messageSidebar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <section className="container grid grid-cols-12">
-      <MessageSideBar />
+      <Suspense fallback={<div>Loading sidebar...</div>}>
+        <MessageSideBar />
+      </Suspense>
       <div className="col-span-8">{children}</div>
     </section>
   );
